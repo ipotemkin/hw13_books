eaa68e015c83d9563903a45923a4797960d795e7
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2021-09-28 00:48:19 +0300

    added a func for an isbn code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import json
 
 app = Flask(__name__)
@@ -17,15 +17,28 @@ def to_json(filename, books):
         json.dump(books, fp, ensure_ascii=False, indent='\t')
 
 
+def gen_isbn(number):
+    s1 = 978
+    s2 = 1
+    s3 = 266
+    s4 = number  # 5 digits
+    s5 = 1  # 1 digit (1-4) - control sum
+    format_isbn = "%3s-%1s-%3s-%05d-%1s"
+    return format_isbn % (s1, s2, s3, s4, s5)
+
+
 @app.route('/books/<uid>')
 def get_book(uid):
     pass
 
 
-@app.route('/', methods=['POST'])
-def create_book(uid):
+@app.route('/create', methods=['POST'])
+def create_book():
+    book = request.get_json()
+
     pass
 
 
 if __name__ == '__main__':
-    app.run()
+    # app.run()
+    print(gen_isbn(17))
