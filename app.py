241e829bd2717c691dba5e97c0967ea0dfa58fde
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2021-09-28 22:24:56 +0300

    Deleted code with global var BOOKS

--- app.py
+++ app.py
@@ -7,23 +7,13 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 BOOKS_FILE = 'books.json'
-USING_RAM = True  # to store all data in RAM in BOOKS
-if USING_RAM:
-    BOOKS = read_json(BOOKS_FILE)
-else:
-    BOOKS = []
-
 HTML_WRAP = '<html lang="ru"><meta charset="UTF-8"><p>{}</p></html>'
 
 
 @app.route('/books/<int:uid>')
 def get_book(uid: int):
-    books = read_json(BOOKS_FILE) if not USING_RAM else BOOKS
+    books = read_json(BOOKS_FILE)
     book = get_book_by_id(uid, books)
-    print('get_book:')  # TODO remove
-    print('books =', books)  # TODO remove
-    print('BOOKS =', BOOKS)  # TODO remove
-    print('BOOKS = books :', BOOKS is books)  # TODO remove
     if not book:
         return Response(HTML_WRAP.format('Нет книги с таким кодом'), content_type='text/html'), 400
     return Response(json.dumps(book, ensure_ascii=False), content_type='application/json'), 200
@@ -32,36 +22,25 @@ def get_book(uid: int):
 
 @app.route('/create', methods=['POST'])
 def create_book():
-    global BOOKS
     new_book = request.get_json()
     if not check_input(new_book):
         message = 'Вы ввели {}. Введите книгу в формате {"name": "название", "author": "автор"}'
         message = message.format(json.dumps(new_book, ensure_ascii=False))
         return Response(HTML_WRAP.format(message), content_type='text/html'), 400
-    books = read_json(BOOKS_FILE) if not USING_RAM else BOOKS
+    books = read_json(BOOKS_FILE)
     new_id = create_new_id(books)
     new_book['id'] = new_id
     new_book['isbn'] = gen_isbn(new_id, new_book['name'])
     books.append(new_book)
-    # if USING_RAM:
-    #     BOOKS = books
     to_json(BOOKS_FILE, books)
-    print('create_book:')  # TODO remove
-    print('books =', books)  # TODO remove
-    print('BOOKS =', BOOKS)  # TODO remove
-    print('BOOKS = books :', BOOKS is books)  # TODO remove
     return Response(json.dumps(new_book, ensure_ascii=False), content_type='application/json'), 201
 
 
 @app.route('/search', methods=['POST'])
 def search_book():
-    books = read_json(BOOKS_FILE) if not USING_RAM else BOOKS
+    books = read_json(BOOKS_FILE)
     what = request.get_json()
     results = search(list(what.values())[0], list(what.keys())[0], books)
-    print('search_book:')  # TODO remove
-    print('books =', books)  # TODO remove
-    print('BOOKS =', BOOKS)  # TODO remove
-    print('BOOKS = books :', BOOKS is books)  # TODO remove
     if not results:
         return Response('По запросу {} ничего не найдено'.format(json.dumps(what, ensure_ascii=False)),
                         content_type='text/html'), 400
@@ -71,12 +50,8 @@ def search_book():
 
 @app.route('/delete/<int:uid>', methods=['DELETE'])
 def delete_book(uid: int):
-    books = read_json(BOOKS_FILE) if not USING_RAM else BOOKS
+    books = read_json(BOOKS_FILE)
     book = get_book_by_id(uid, books)
-    print('delete_book:')  # TODO remove
-    print('books =', books)  # TODO remove
-    print('BOOKS =', BOOKS)  # TODO remove
-    print('BOOKS = books :', BOOKS is books)  # TODO remove
     if not book:
         return Response(HTML_WRAP.format('Нет книги с таким кодом'), content_type='text/html'), 400
     books.remove(book)
@@ -85,19 +60,4 @@ def delete_book(uid: int):
 
 
 if __name__ == '__main__':
-    # if USING_RAM:
-    #     BOOKS = read_json(BOOKS_FILE)
     app.run()
-
-    # DEBUG
-    # print(gen_isbn(17))
-    # print(create_new_id([{'id': 10}]))
-
-    # print(check_input({}))
-    # print(check_input([]))
-    # print(check_input({"name": "Name"}))
-    # print(check_input({"name": "Name", "author": "Author"}))
-
-    # print(hash("Конан Дойль"))
-    # print(gen_isbn(1))
-    # print(gen_isbn(1, "Конан Дойль"))
